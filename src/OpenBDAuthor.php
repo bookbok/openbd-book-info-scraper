4256f28c0a60a3519a1af13954d13785b32d318d
kento-oka <kento-oka@kentoka.com>
2020-08-09 17:38:44 +0900

    新しいインターフェースを使うように。このライブラリでこのオブジェクトたちに追加実装する必要はない

--- src/OpenBDAuthor.php
+++ src/OpenBDAuthor.php
@@ -1,24 +1,12 @@
 <?php
 
-/**
- * bookbok/openbd-book-info-scraper
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright (c) BookBok
- * @license MIT
- * @since 1.0.0
- */
-
 namespace BookBok\BookInfoScraper\OpenBD;
 
-use BookBok\BookInfoScraper\Information\Author;
+use BookBok\BookInfoScraper\Information\ImmutableAuthor;
 
 /**
  *
  */
-class OpenBDAuthor extends Author
+class OpenBDAuthor extends ImmutableAuthor
 {
 }
--- src/OpenBDBook.php
+++ src/OpenBDBook.php
@@ -1,74 +1,9 @@
 <?php
 
-/**
- * bookbok/openbd-book-info-scraper
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright (c) BookBok
- * @license MIT
- * @since 1.0.0
- */
-
 namespace BookBok\BookInfoScraper\OpenBD;
 
-use BookBok\BookInfoScraper\Information\Book;
+use BookBok\BookInfoScraper\Information\ImmutableBook;
 
-/**
- *
- */
-class OpenBDBook extends Book
+class OpenBDBook extends ImmutableBook
 {
-    public const ONIX = "onix";
-    public const HANMOTO = "hanmoto";
-    public const SUMMARY = "summary";
-
-    /**
-     * @var mixed[]
-     */
-    private $data;
-
-    /**
-     * Constructor.
-     *
-     * @param mixed[] $data レスポンスデータ
-     */
-    public function __construct(array $data)
-    {
-        $this->data = $data;
-
-        parent::__construct(
-            $this->get("RecordReference"),
-            $this->get("DescriptiveDetail.TitleDetail.TitleElement.TitleText.content")
-        );
-    }
-
-    /**
-     * APIからのレスポンスデータを取得する。
-     *
-     * @param string $accessKey レスポンスデータネームスペース以下のキー
-     * @param string $source レスポンスデータのネームスペース
-     *
-     * @return mixed|null
-     */
-    public function get(string $accessKey, string $source = self::ONIX)
-    {
-        $data = $this->data[$source] ?? null;
-
-        foreach (explode(".", $accessKey) as $key) {
-            if (!is_array($data) || !isset($data[$key])) {
-                return null;
-            }
-
-            $data = $data[$key];
-        }
-
-        if ("" === $data) {
-            return null;
-        }
-
-        return $data;
-    }
 }
