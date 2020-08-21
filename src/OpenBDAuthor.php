c295818736fdf9b2af2f8e181774a220b2415c4a
kento-oka <kento-oka@kentoka.com>
2020-08-22 03:26:33 +0900

    著者略歴を追加

--- src/OpenBDAuthor.php
+++ src/OpenBDAuthor.php
@@ -4,9 +4,33 @@ namespace BookBok\BookInfoScraper\OpenBD;
 
 use BookBok\BookInfoScraper\Information\ImmutableAuthor;
 
-/**
- *
- */
 class OpenBDAuthor extends ImmutableAuthor
 {
+    /** @var string|null */
+    protected $biography;
+
+    /**
+     * 著者略歴を設定した新しいインスタンスを返す。
+     *
+     * @param string|null $biography 著者略歴。
+     *
+     * @return OpenBDAuthor
+     */
+    public function withBiography(?string $biography): OpenBDAuthor
+    {
+        $convertedBiography = $biography;
+
+        if ($biography !== null) {
+            if ($biography === "") {
+                throw new \InvalidArgumentException("Cannot set empty string.");
+            }
+
+            $convertedBiography = preg_replace("/\r\n|\r|\n/", "\n", $biography);
+        }
+
+        $cloneAuthor = clone $this;
+        $cloneAuthor->biography = $convertedBiography;
+
+        return $cloneAuthor;
+    }
 }
